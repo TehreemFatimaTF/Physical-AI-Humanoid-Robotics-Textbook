6ffa2961d3ef8b1053fdf376d3d84898f44d83c1
Tehreem Fatima <tf797855@gmail.com>
2025-12-12 17:11:26 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -207,7 +207,7 @@ const HeroSection = () => {
           }}
         >
           <img 
-            src="https://png.pngtree.com/png-vector/20240320/ourmid/pngtree-curious-robot-reading-book-png-image_12181635.png" 
+            src="https://pngtree.com/freepng/a-robot-with-blue-eye-pointing-transparent-background_15701772.html" 
             alt="Robot Reading Book"
             className="robot-image"
           />
